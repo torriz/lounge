ce09f7ac6c38ef7e0635b946d4b71981a20a6c1d
torriz <torriz@users.noreply.github.com>
2016-06-30 03:37:20 +0200

    Create amsg.js

---
+++ src/plugins/inputs/amsg.js
@@ -0,0 +1,28 @@
+xports.commands = ["amsg"];
+
+exports.input = function(network, chan, cmd, args) {
+        if (args.length === 0 || args[0] === "") {
+                return true;
+        }
+
+        var irc = network.irc;
+        var msg = args.join(" ");
+
+        network.channels.forEach(function(chan) {
+                if (chan.type !== "channel") {
+                        return;
+                }
+
+                irc.say(chan.name, msg);
+
+                if (!irc.network.cap.isEnabled("echo-message")) {
+                        irc.emit("privmsg", {
+                                nick: irc.user.nick,
+                                target: chan.name,
+                                message: msg
+                        });
+                }
+        });
+
+        return true;
+};
